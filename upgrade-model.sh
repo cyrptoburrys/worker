d93515af383042bb16b9f78a93840806b04a29c8
cyrptoburrys <136279935+cyrptoburrys@users.noreply.github.com>
2024-09-10 08:06:39 +0300

    upgrade-model.sh

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -2,40 +2,30 @@
 
 BOLD="\033[1m"
 UNDERLINE="\033[4m"
-LIGHT_BLUE="\033[1;34m"     # Light Blue for primary messages
-BRIGHT_GREEN="\033[1;32m"   # Bright Green for success messages
-MAGENTA="\033[1;35m"        # Magenta for titles
-RESET="\033[0m"             # Reset to default color
+LIGHT_BLUE="\033[1;34m"
+BRIGHT_GREEN="\033[1;32m"
+MAGENTA="\033[1;35m"
+RESET="\033[0m"
 
 echo -e "${LIGHT_BLUE}Upgrade Your Allora Model(TYPE:Y):${RESET}"
 read -p "" installdep
 echo
 
 if [[ "$installdep" =~ ^[Yy]$ ]]; then
-
     echo -e "${LIGHT_BLUE}Clone & Replace old file :${RESET}"
-    echo
     rm -rf app.py
     rm -rf requirements.txt
-    wget -q https://raw.githubusercontent.com/0xtnpxsgt/Allora-Comprehensive-Guide/BiLSTM/app.py -O /root/allora-huggingface-walkthrough/app.py
-    wget -q https://raw.githubusercontent.com/0xtnpxsgt/Allora-Comprehensive-Guide/BiLSTM/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
-    wget -q https://github.com/0xtnpxsgt/Allora-Comprehensive-Guide/raw/BiLSTM/enhanced_bilstm_model.pth -O /root/allora-huggingface-walkthrough/birnn_model_optimized.pth
+    wget -q https://raw.githubusercontent.com/your_repo/app.py -O /root/allora-huggingface-walkthrough/app.py
+    wget -q https://raw.githubusercontent.com/your_repo/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
+    wget -q https://github.com/your_repo/final_tft_model.pth -O /root/allora-huggingface-walkthrough/final_tft_model.pth
     wait
-	
+    
     echo -e "${LIGHT_BLUE}Rebuild and run a model :${RESET}"
-
-    cd /root/allora-huggingface-walkthrough/	
-    echo
+    cd /root/allora-huggingface-walkthrough/
     docker compose up --build -d
-    echo
-	
-    echo
     docker compose logs -f
-    echo
-	
 else
     echo -e "${BRIGHT_GREEN}Operation Canceled :${RESET}"
-    
 fi
 
 echo
