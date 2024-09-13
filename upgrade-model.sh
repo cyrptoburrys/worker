73ab17444a3f130778306db207e07031cdde509f
cyrptoburrys <136279935+cyrptoburrys@users.noreply.github.com>
2024-09-13 22:09:03 +0300

    Update upgrade-model.sh

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -12,21 +12,21 @@ read -p "" installdep
 echo
 
 if [[ "$installdep" =~ ^[Yy]$ ]]; then
-    echo -e "${LIGHT_BLUE}Clone & Replace old file :${RESET}"
+    echo -e "${LIGHT_BLUE}Cloning & Replacing old files...${RESET}"
     rm -rf app.py
-    rm -rf requirements.txt
-    wget -q https://raw.githubusercontent.com/your_repo/app.py -O /root/allora-huggingface-walkthrough/app.py
-    wget -q https://raw.githubusercontent.com/your_repo/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
-    wget -q https://github.com/your_repo/final_tft_model.pth -O /root/allora-huggingface-walkthrough/final_tft_model.pth
+    rm -rf EnhancedPricePredictor.py
+    rm -rf final_tft_model.pth
+    wget -q https://raw.githubusercontent.com/cyrptoburrys/worker/main/app.py -O /root/allora-huggingface-walkthrough/app.py
+    wget -q https://raw.githubusercontent.com/cyrptoburrys/worker/main/EnhancedPricePredictor.py -O /root/allora-huggingface-walkthrough/EnhancedPricePredictor.py
+    wget -q https://github.com/cyrptoburrys/worker/main/final_tft_model.pth -O /root/allora-huggingface-walkthrough/final_tft_model.pth
     wait
     
-    echo -e "${LIGHT_BLUE}Rebuild and run a model :${RESET}"
+    echo -e "${LIGHT_BLUE}Rebuilding and running the model...${RESET}"
     cd /root/allora-huggingface-walkthrough/
     docker compose up --build -d
     docker compose logs -f
 else
-    echo -e "${BRIGHT_GREEN}Operation Canceled :${RESET}"
+    echo -e "${BRIGHT_GREEN}Operation Canceled.${RESET}"
 fi
 
-echo
 echo -e "${MAGENTA}==============0xTnpxSGT | Allora===============${RESET}"
