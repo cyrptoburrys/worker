7ae8dd6c9938a6ee5f671a7147904cd79c3722c1
cyrptoburrys <136279935+cyrptoburrys@users.noreply.github.com>
2024-09-15 18:38:07 +0300

    Update upgrade-model.sh

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -21,7 +21,7 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
     wget -q https://raw.githubusercontent.com/cyrptoburrys/worker/main/EnhancedPricePredictor.py -O /root/allora-huggingface-walkthrough/EnhancedPricePredictor.py
     wget -q https://github.com/cyrptoburrys/worker/main/final_tft_model.pth -O /root/allora-huggingface-walkthrough/final_tft_model.pth
     wget -q https://github.com/cyrptoburrys/worker/main/final_tft_model.pth -O /root/allora-huggingface-walkthrough/birnn_model_optimized.pth
-    wget -q https://github.com/cyrptoburrys/worker/main/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
+    wget -q https://raw.githubusercontent.com/cyrptoburrys/worker/main/requirements.txt -O /root/allora-huggingface-walkthrough/requirements.txt
     wait
     
     echo -e "${LIGHT_BLUE}Rebuilding and running the model...${RESET}"
